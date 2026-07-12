eaf3d081b5e6a503c06ea05dbcf136a359c256d6
Marquis Persley <Marquisster@gmail.com>
2026-07-11 17:30:06 -0700

    Create initial mock telemetry engine

---
+++ src/audi_telemetry/telemetry_engine.py
@@ -0,0 +1,10 @@
+engine_rpm: int = 850
+vehicle_speed_mph: int = 0
+coolant_temperature_f: int = 190
+battery_voltage: float = 12.6
+
+print(engine_rpm)
+print(vehicle_speed_mph)
+print(coolant_temperature_f)
+print(battery_voltage)
+
